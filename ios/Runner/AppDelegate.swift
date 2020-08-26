0b4c0a424da7dd6dd916792281da66133bb51fab
Ekram Bhuiyan <ekrambhuiyan@Ekrams-MacBook-Pro.local>
2020-08-26 18:18:49 -0400

    Added some modifications  and clarifying comments to the app Delegate class which sets up the logic for how responses from the flutter client will be handled.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,32 +1,47 @@
 import UIKit
 import Flutter
 
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  
-  private func receiveAccelerometerData(result: FlutterResult) {
-      result(FlutterError(code: "UNAVAILABLE",
-                          message: "accelerometer info unavailable",
-                          details: nil))
-  }
 
-  override func application(
+  // These are the references for managers handling how to retreive the results required by a client
+  
+  // 1. Accelerometer manager which processes raw accelerometer results retreived from a device
+  let accManager = AccManager()
+  
+  // 2.
+  // ....
+
+
+  override func application (
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-    
-    let accelerometerDataChannel = FlutterMethodChannel(name: "flutter.prod/accelerometerData",
-                                              binaryMessenger: controller.binaryMessenger)
+    // Logic backing the computed property that determines the truthiness of if the Swift application was
+    // launched is defined here:
 
-    accelerometerDataChannel.setMethodCallHandler({
-      [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
-        // Note: this method is invoked on the UI thread
+    // 1. Set up main ViewController and accelerometer data channel to receive from
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let accDataChannel = FlutterMethodChannel(name: "flutter.dev/accelerometerData",
+                                              binaryMessenger: controller.binaryMessenger)
+  
+    // 2. Define event handler to process any queued messages that can now be received
+    // with a channel reference being defined
+
+    accDataChannel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: FlutterResult) -> Void in
         if (call.method == "getAccelerometerData") {
-          self?.receiveAccelerometerData(result: result)
           return
         }
-        result(FlutterMethodNotImplemented)
+        // 1. Run the logic to access the raw results returned from accessing the sensors on
+        // the device (currently the value type assigned to raw results is just a placeholder)
+        let rawResults = FlutterError(code: "UNAVAILABLE",
+                              message: "accelerometer info unavailable",
+                              details: nil)
+
+         // 2. Manage and process all the raw results that have now been returned
+        result(self.accManager.processAccData(accResults: rawResults))
     })
 
     GeneratedPluginRegistrant.register(with: self)
@@ -34,3 +49,16 @@ import Flutter
   }
 }
 
+// Add setup for different implementations???
+
+public class AccManager {
+      
+      public func processAccData(accResults rawResults: FlutterError) -> FlutterError{
+            // 1. Process Acc Results
+            return rawResults
+      }
+
+      init() {
+
+      }
+}
